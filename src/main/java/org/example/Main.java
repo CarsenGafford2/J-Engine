e1ab42916c322dac23dca8d4be1723869cad066f
CarsenGafford2 <carsengafford2@gmail.com>
2025-10-20 09:45:50 -0600

    Add class-level Javadoc to Main and MainController
    Added Javadoc comments to Main and MainController classes, including author and version information for better documentation and code clarity.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main Class, Loads up the FXML document.
+ * @author Carsen Gafford
+ * @version v0.1.0
+ */
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -5,6 +5,11 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * Main controller for handling inputs and things on the FXML application.
+ * @author Carsen Gafford
+ * @version v0.1.0
+ */
 public class MainController {
     @FXML
     private Canvas canvas;
